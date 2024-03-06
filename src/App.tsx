c621ecac7553fd33e381071e4c62fdd307618990
Auriel404DH <nspmsoul16@gmail.com>
2024-03-06 14:14:06 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import styles from './App.module.css';
+
+interface Param {
+  id: number;
+  name: string;
+}
+
+interface ParamValue {
+  paramId: number;
+  value: string;
+}
+interface Model {
+  paramValues: ParamValue[];
+  params: Param[]
+}
+
+const params: Param[] = [
+  {
+    id: 1,
+    name: "Назначение"
+  },
+  {
+    id: 2,
+    name: "Длинна"
+  }
+]
+
+const emptyModel = {
+  params: params,
+  paramValues: params.map(elem => ({ paramId: elem.id, value: '' }))
+}
 
 function App() {
+  const [model, setModel] = useState<Model>(emptyModel)  
+
+  const onChangeModel = ({ id, value }: { id: number, value: string }) => {
+      const currentParam = model.paramValues.find(param => param.paramId === id)
+      
+      if (!currentParam) return;      
+
+      const newParam = {...currentParam, value}
+
+      const otherModelParams = model.paramValues.filter(param => param.paramId !== id)
+
+      setModel({...model, params, paramValues: [...otherModelParams, newParam]})
+  }
+
+  const getModel = () => console.log(model);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.wrapper}>
+      {params.map((elem)=> {
+        return (
+          <div className={styles.paramItem}>
+            <div key={elem.id}>{elem.name}</div>
+            <input onChange={(e) => onChangeModel({id: elem.id, value: e.target.value})} type='text'/>
+          </div>
+        )
+      })}
+
+      <button onClick={getModel}>Показать модель</button>
     </div>
   );
 }
